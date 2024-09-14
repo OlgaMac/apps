f96b63bf1b3f8c381b96c9c4db6be19b0c8bfc19
OlgaMac <oljamackeewa@gmail.com>
2024-09-15 00:17:05 +0300

    flyway

--- webbooks/src/main/resources/db/migration/V20240901_chenge_constrant_age.sql
+++
@@ -1,5 +0,0 @@
--- Удаляем старое ограничение
-ALTER TABLE clients DROP CONSTRAINT clients_age_check;
-
--- Добавляем новое ограничение
-ALTER TABLE clients ADD CONSTRAINT clients_age_check CHECK (age > 0 AND age <= 150);
