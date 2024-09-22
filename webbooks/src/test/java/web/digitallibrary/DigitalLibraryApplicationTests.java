e2cdee5e56dd10650e7a935ff0bfa3a52f5f6bdf
OlgaMac <oljamackeewa@gmail.com>
2024-09-22 21:32:58 +0300

    fix test

--- webbooks/src/test/java/web/digitallibrary/DigitalLibraryApplicationTests.java
+++ webbooks/src/test/java/web/digitallibrary/DigitalLibraryApplicationTests.java
@@ -1,9 +1,12 @@
 package web.digitallibrary;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
 class DigitalLibraryApplicationTests {
 
     @Test
